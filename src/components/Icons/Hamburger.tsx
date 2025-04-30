4a783e00a24ae96bd857d82c57ed7b064140c1ea
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2025-04-30 21:16:35 +0300

    fix: Hamburger menus wasn't showing up on small screens.

--- src/components/Icons/Hamburger.tsx
+++ src/components/Icons/Hamburger.tsx
@@ -1,6 +1,6 @@
 export default function Hamburger() {
   return (
-    <button className="hidden cursor-pointer flex-col justify-center gap-y-1.75">
+    <button className="max-lg:flex hidden cursor-pointer flex-col justify-center gap-y-1.75">
       <div className="h-[0.15rem] w-7 shrink-0 grow-0 rounded-3xl bg-black" />
       <div className="h-[0.15rem] w-7 shrink-0 grow-0 rounded-3xl bg-black" />
       <div className="h-[0.15rem] w-7 shrink-0 grow-0 rounded-3xl bg-black" />
