b33e118054465f9135a89b67d2ec332f51672024
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2025-04-30 16:37:07 +0300

    feat: made all the website responsive on all screens.

--- src/components/ExploreMore/ExploreMore.tsx
+++ src/components/ExploreMore/ExploreMore.tsx
@@ -1,76 +1,67 @@
 import { useState } from "react";
-import CaretUp from "../Icons/CaretUp";
-import LocationCard from "./LocationCard";
 import { LOCATION_CARDS_SHOWN } from "../../utils/constants";
-import useQueryLocations from "../../hooks/useQueryLocations";
+import LocationCard from "./LocationCard";
+import CaretUp from "../Icons/CaretUp";
 import Loader from "../Loader";
 import Error from "../Error";
+import useQueryLocations from "../../hooks/useQueryLocations";
 
 export default function ExploreMore() {
   const { locations, error, isLoading } = useQueryLocations();
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const renderLocations = locations?.slice(
-    currentIndex,
-    currentIndex + LOCATION_CARDS_SHOWN,
-  );
+
+  const [currIndex, setCurrIndex] = useState<number>(0);
+
   const totalLocations = locations?.length || 0;
+  const renderedLocations = locations?.slice(
+    currIndex,
+    currIndex + LOCATION_CARDS_SHOWN,
+  );
 
-  const handleRightClick = () => {
-    setCurrentIndex((prevIndex) => prevIndex + 1);
-  };
+  const handleRightClick = () => setCurrIndex((prevIndex) => prevIndex + 1);
+  const handleLeftClick = () => setCurrIndex((prevIndex) => prevIndex - 1);
 
-  const handleLeftClick = () => {
-    setCurrentIndex((prevIndex) => prevIndex - 1);
-  };
   return (
-    <section className="px-45.5 py-26" id="ExploreMore">
-      <div className="m-auto max-w-389">
-        {/* Title and Arrows*/}
-        <div className="flex items-end justify-between">
-          {/* Title */}
+    <section
+      className="max-3xl:px-20 px-24 py-26 max-2xl:px-14 max-xl:px-10 max-lg:px-6 max-lg:py-24 max-sm:px-4"
+      id="ExploreMore"
+    >
+      <div className="max-mdlg:max-w-lg m-auto max-w-389">
+        <div className="flex items-end justify-between gap-y-33 max-sm:flex-wrap max-sm:gap-x-16 max-sm:gap-y-10">
           <div>
-            <h2 className="tracking-6 mb-4 text-[3.25rem] font-semibold">
+            <h2 className="tracking-6 max-3xl:text-[2.875rem] mb-4 text-[3.25rem] font-semibold max-2xl:text-[2.625rem] max-xl:text-[2.25rem] max-md:mb-3 max-sm:text-[2rem]">
               Explore more
             </h2>
-            <p className="tracking-6 text-grey-700 text-[1.75rem] font-light">
-              Let’s go on an adventure
+            <p className="tracking-6 text-grey-700 max-3xl:text-[1.5rem] text-[1.75rem] font-light max-2xl:text-[1.375rem] max-xl:text-[1.25rem] max-md:text-lg max-sm:text-base">
+              Let’s go on adventure
             </p>
           </div>
-
-          {/* Arrows */}
-          <div className="mb-2 flex gap-x-6">
+          <div className="max-3xl:mb-1.5 max-3xl:gap-x-5 mb-2 flex gap-x-6 max-2xl:gap-x-4 max-xl:mb-0.5 max-sm:gap-x-3">
             <button
+              className="bg-grey-300 not-disabled:hover:bg-grey-400 flex size-18 cursor-pointer place-content-center rounded-full transition-all duration-200 disabled:cursor-not-allowed disabled:opacity-50 max-2xl:size-17 max-xl:size-16 max-sm:size-15"
+              aria-label="arrow left"
               onClick={handleLeftClick}
-              disabled={currentIndex === 0}
-              aria-label="Arrow Left"
-              className="bg-grey-300 flex size-18 cursor-pointer place-content-center rounded-full transition-all duration-200 not-disabled:hover:bg-gray-400 disabled:cursor-not-allowed disabled:opacity-50"
+              disabled={currIndex === 0}
             >
               <CaretUp className="w-6 -rotate-90 fill-white" />
             </button>
             <button
+              className="bg-primary-700 not-disabled:hover:bg-primary-800 flex size-18 cursor-pointer place-content-center rounded-full transition-all duration-200 disabled:cursor-not-allowed disabled:opacity-50 max-2xl:size-17 max-xl:size-16 max-sm:size-15"
+              aria-label="arrow right"
               onClick={handleRightClick}
-              disabled={currentIndex >= totalLocations - 6}
-              aria-label="Arrow right"
-              className="bg-primary-700 not-disabled:hover:bg-primary-800 flex size-18 cursor-pointer place-content-center rounded-full transition-all duration-200 disabled:cursor-not-allowed disabled:opacity-50"
+              disabled={currIndex >= totalLocations - 6}
             >
               <CaretUp className="w-6 rotate-90 fill-white" />
             </button>
           </div>
         </div>
-
-        {/* Loading State */}
         {isLoading && !error && <Loader />}
-
-        {/* Success State */}
         {!isLoading && !error && (
-          <ul className="mt-33 grid grid-cols-3 gap-x-29 gap-y-24">
-            {renderLocations?.map((location) => (
+          <ul className="max-3xl:mt-31 max-3xl:gap-x-26 mt-33 grid grid-cols-3 gap-x-29 gap-y-24 max-2xl:mt-24 max-2xl:gap-x-16 max-2xl:gap-y-20 max-xl:mt-20 max-xl:grid-cols-2 max-xl:gap-x-22 max-lg:gap-x-12 max-md:mt-16 max-md:grid-cols-1 max-sm:gap-y-16">
+            {renderedLocations?.map((location) => (
               <LocationCard location={location} key={location.id} />
             ))}
           </ul>
         )}
-
-        {/* Error State */}
         {!isLoading && error && (
           <Error>
             It looks like something went wrong while loading our travel
--- src/components/FrequentTraveler.tsx
+++ src/components/FrequentTraveler.tsx
@@ -1,38 +1,48 @@
-import { useState, MouseEvent } from "react";
-import Checkmark from "./Icons/Checkmark";
-import { useFormAndValidation } from "../hooks/useFormAndValidation";
+import { MouseEvent, useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
+import { useFormAndValidation } from "../hooks/useFormAndValidation";
+import Checkmark from "./Icons/Checkmark";
 import useInsertLead from "../hooks/useInsertLead";
+import { FORM_STATE_DURATION } from "../utils/constants";
 
-interface formState {
+interface FormState {
   currentState: "idle" | "pending" | "success" | "error";
   errorMessage: string | null;
 }
 
-export default function FrequentTraveler() {
-  const [isChecked, setIsChecked] = useState<boolean>(false);
-  const { values, errors, isValid, handleChange, resetForm } =
-    useFormAndValidation({ fullName: "", emailAddress: "" });
-  const [formState, setFormState] = useState<formState>({
+const buttonStateClasses = {
+  idle: "bg-primary-700 opacity-100",
+  pending: "bg-primary-700 opacity-50",
+  success: "bg-green opacity-100",
+  error: "bg-red opacity-100",
+};
+
+export default function FrequentTravelers() {
+  const [formState, setFormState] = useState<FormState>({
     currentState: "idle",
     errorMessage: null,
   });
-  const buttonStateClasses = {
-    idle: "bg-primary-700 opacity-100",
-    pending: "bg-primary-700 opacity-50",
-    success: "bg-green opacity-100",
-    error: "bg-red opacity-100",
-  };
+
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const { values, handleChange, errors, isValid, resetForm } =
+    useFormAndValidation({
+      emailAddress: "",
+      fullName: "",
+    });
 
   const mutation = useInsertLead({
     onSuccess: handleSuccess,
     onError: handleError,
   });
 
-  function handleSubmit(e: MouseEvent<HTMLButtonElement>) {
+  async function handleSubmit(e: MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
     if (isChecked && isValid) {
+      // we are now entering  a pending..
       setFormState({ currentState: "pending", errorMessage: null });
+
+      // lets attempt to insert lead data
       mutation.mutate({
         createdAt: Date.now(),
         fullName: values.fullName,
@@ -45,54 +55,53 @@ export default function FrequentTraveler() {
     resetForm();
     setIsChecked(false);
     setFormState({ currentState: "success", errorMessage: null });
-    setTimeout(() => {
-      setFormState({ currentState: "idle", errorMessage: null });
-    }, 1750);
+
+    // Tell the browser to run this function after 1250 ms, reset form state
+    setTimeout(
+      () => setFormState({ currentState: "idle", errorMessage: null }),
+      FORM_STATE_DURATION,
+    );
   }
 
   function handleError(error: Error) {
     setFormState({ currentState: "error", errorMessage: error.message });
-    setTimeout(() => {
-      setFormState({ currentState: "idle", errorMessage: null });
-    }, 3000);
+
+    setTimeout(
+      () => setFormState({ currentState: "idle", errorMessage: null }),
+      FORM_STATE_DURATION,
+    );
   }
 
   return (
-    <section className="bg-primary-100 px-45.5 py-36">
-      <div className="m-auto flex max-w-389 items-center justify-between gap-x-28 border-y-1 border-y-gray-500/40 py-26">
-        {/* Left Column */}
-        <div className="basis-150 text-center">
-          <h3 className="tracking-6 mb-9.5 text-[1.75rem]/14 font-semibold">
+    <section className="bg-primary-100 max-3xl:px-20 max-3xl:py-34 px-24 py-36 max-2xl:px-14 max-2xl:py-28 max-xl:px-10 max-xl:py-26 max-lg:px-6 max-lg:pt-24 max-lg:pb-32 max-md:pb-28 max-sm:px-4">
+      <div className="border-y-grey-500/40 m-auto flex flex-col lg:flex-row max-w-389 items-center justify-between gap-x-28 border-y-1 py-26 max-2xl:gap-x-20 max-2xl:py-20 max-xl:gap-x-10 max-xl:py-16 max-lg:max-w-lg max-lg:flex-col max-lg:gap-y-16 max-lg:border-none max-lg:py-0">
+        <div className="basis-150 text-center max-lg:basis-auto">
+          <h3 className="tracking-6 max-3xl:text-[1.5rem]/12 max-3xl:mb-8 mb-9.5 text-[1.75rem]/14 font-semibold max-md:mb-9.5">
             Learn About Our Frequent Traveler Program
           </h3>
-          <p className="text-base/13.5 text-gray-800">
+          <p className="text-grey-800 text-base/13.5">
             Interested in saving up to $1000 on your next vacation? How about
             earning travel points that can be converted into rewards like extra
             nights, free meals, and exclusive offers from resorts around globe?
           </p>
         </div>
-
-        {/* Divider */}
-        <div className="block w-0.25 self-stretch bg-gray-500/40" />
-
-        {/* Right Column */}
-        <form className="flex basis-150 flex-col">
-          {/* Name */}
-          <label className="mb-8">
-            <p className="tracking-6 mb-3 text-lg/9.5 font-semibold">
+        <div className="bg-grey-500/40 block w-0.25 self-stretch max-lg:hidden" />
+        <form className="flex basis-150 flex-col max-lg:basis-auto">
+          <label className="max-3xl:mb-7 mb-8 max-md:mb-8">
+            <p className="tracking-6 max-3xl:mb-2.5 mb-3 text-lg/9.5 font-semibold max-2xl:text-base max-md:mb-3">
               Full Name
             </p>
             <input
               required
               type="text"
               name="fullName"
+              value={values.fullName}
+              onChange={handleChange}
               minLength={2}
               maxLength={50}
-              placeholder="Jane Doe"
-              onChange={handleChange}
               disabled={formState.currentState !== "idle"}
-              value={values.fullName}
-              className={`placeholder:text-grey-400 w-full rounded-lg bg-white py-3.5 pl-4 transition-all duration-200 placeholder:font-light focus:outline-1 disabled:opacity-50 ${errors.fullName && "outline-red"} `}
+              placeholder="Jane Doe"
+              className={`placeholder:text-grey-400 w-full rounded-lg bg-white py-3.5 pl-4 transition-all duration-200 placeholder:font-light focus:outline-1 disabled:opacity-50 max-sm:py-4 max-sm:text-sm ${errors.fullName && "outline-red"}`}
             />
             <AnimatePresence>
               {errors.fullName && (
@@ -108,21 +117,21 @@ export default function FrequentTraveler() {
               )}
             </AnimatePresence>
           </label>
-
-          {/* Email */}
-          <label className="mb-12">
-            <p className="tracking-6 mb-3 text-lg/9.5 font-semibold">Email</p>
+          <label className="max-3xl:mb-11.5 mb-12 max-md:mb-16">
+            <p className="tracking-6 max-3xl:mb-2.5 mb-3 text-lg/9.5 font-semibold max-2xl:text-base max-md:mb-3">
+              Email
+            </p>
             <input
               required
+              type="email"
               name="emailAddress"
+              value={values.emailAddress}
+              onChange={handleChange}
               minLength={3}
               maxLength={50}
-              type="email"
-              placeholder="janedoe@gmail.com"
-              onChange={handleChange}
               disabled={formState.currentState !== "idle"}
-              value={values.emailAddress}
-              className={`placeholder:text-grey-400 w-full rounded-lg bg-white py-3.5 pl-4 transition-all duration-200 placeholder:font-light focus:outline-1 disabled:opacity-50 ${errors.emailAddress && "outline-red"}`}
+              placeholder="janedoe@gmail.com"
+              className={`placeholder:text-grey-400 w-full rounded-lg bg-white py-3.5 pl-4 transition-all duration-200 not-hover:transition-none placeholder:font-light focus:outline-1 disabled:opacity-50 max-sm:py-4 max-sm:text-sm ${errors.emailAddress && "outline-red"}`}
             />
             <AnimatePresence>
               {errors.emailAddress && (
@@ -138,51 +147,33 @@ export default function FrequentTraveler() {
               )}
             </AnimatePresence>
           </label>
-
-          {/* Checkbox */}
-          <div className="flex flex-wrap items-center justify-between gap-8">
-            <label className="text-grey-800f flex cursor-pointer items-center gap-x-1.5">
+          <div className="flex flex-wrap items-center justify-between gap-x-8 gap-y-8">
+            <label className="text-grey-800 flex cursor-pointer items-center gap-x-1.5">
               <button
-                className="flex size-5 cursor-pointer items-center justify-center rounded-xs bg-white p-1 disabled:opacity-50"
+                className="flex size-5 cursor-pointer items-center justify-center rounded-xs bg-white p-1 disabled:opacity-50 max-xl:size-4.5"
                 type="button"
-                onClick={() => setIsChecked(!isChecked)}
-                disabled={formState.currentState !== "idle"}
+                onClick={() => setIsChecked((c) => !c)}
+                disabled={formState.currentState === "pending"}
               >
                 <Checkmark
-                  className={`size-2 transition-all duration-200 ${isChecked ? "visible size-3 opacity-100" : "invisible size-2 opacity-0"}`}
+                  className={`transition-all duration-200 ${isChecked ? "visible size-3 opacity-100" : "invisible size-2 opacity-0"}`}
                 />
               </button>
-              <p className="text-sm tracking-[.03rem]">
+              <p className="text-sm tracking-[.03rem] max-xl:text-xs">
                 Agree to receive promotional email updates
               </p>
             </label>
-
-            {/* Submit */}
-            <button
-              className={`enabled:hover:bg-primary-800 cursor-pointer rounded-[0.625rem] px-8 py-3.5 font-medium text-white transition-all duration-200 disabled:cursor-not-allowed ${buttonStateClasses[formState.currentState]}`}
+            <motion.button
               onClick={handleSubmit}
               disabled={formState.currentState !== "idle"}
+              className={`not-disabled:hover:bg-primary-800 cursor-pointer rounded-[.625rem] px-8 py-3.5 font-medium text-white transition-all duration-200 disabled:cursor-not-allowed max-xl:py-3 max-xl:text-sm max-xl:font-normal max-lg:py-3.5 max-lg:text-base ${buttonStateClasses[formState.currentState]}`}
             >
               {formState.currentState === "idle" && "Learn More"}
               {formState.currentState === "pending" && "Submitting..."}
-              {formState.currentState === "success" && "Success!"}
               {formState.currentState === "error" && "Submission Failed"}
-            </button>
+              {formState.currentState === "success" && "Success!"}
+            </motion.button>
           </div>
-          {/* Error State */}
-          {formState.currentState === "error" && (
-            <AnimatePresence>
-              <motion.p
-                initial={{ opacity: 0, height: 0 }}
-                animate={{ opacity: 1, height: "auto" }}
-                exit={{ opacity: 0, height: 0 }}
-                transition={{ duration: 0.15 }}
-                className="text-red mt-5 rounded-xl bg-red-200 px-5 py-2.5 font-medium"
-              >
-                {formState.errorMessage}
-              </motion.p>
-            </AnimatePresence>
-          )}
         </form>
       </div>
     </section>
